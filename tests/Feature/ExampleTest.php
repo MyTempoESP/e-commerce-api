2c21f0ecc5ff87855b6a6d2b6bc74d91e1c343b5
TsukiGva2 <tsukigva@gmail.com>
2025-06-03 20:55:58 -0300

    fix tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,16 +4,18 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+	use RefreshDatabase;
 
-        $response->assertStatus(200);
-    }
+	/**
+	 * A basic test example.
+	 */
+	public function test_the_application_returns_a_successful_response(): void
+	{
+		$response = $this->get('/products');
+		$response->assertStatus(200);
+	}
 }
