fe90cc5766496df61af77d3ce361225adc9e1e1e
TsukiGva2 <tsukigva@gmail.com>
2025-06-10 16:07:23 -0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -3,17 +3,17 @@
 return [
 
 	/*
-		  |--------------------------------------------------------------------------
-		  | Cross-Origin Resource Sharing (CORS) Configuration
-		  |--------------------------------------------------------------------------
-		  |
-		  | Here you may configure your settings for cross-origin resource sharing
-		  | or "CORS". This determines what cross-origin operations may execute
-		  | in web browsers. You are free to adjust these settings as needed.
-		  |
-		  | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-		  |
-		  */
+				|--------------------------------------------------------------------------
+				| Cross-Origin Resource Sharing (CORS) Configuration
+				|--------------------------------------------------------------------------
+				|
+				| Here you may configure your settings for cross-origin resource sharing
+				| or "CORS". This determines what cross-origin operations may execute
+				| in web browsers. You are free to adjust these settings as needed.
+				|
+				| To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+				|
+				*/
 
 	'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
@@ -21,7 +21,7 @@ return [
 
 	'allowed_origins' => ['*'],
 
-	'allowed_origins_patterns' => ['http://127.0.0.1:8000'],
+	'allowed_origins_patterns' => ['http://localhost:8081'],
 
 	'allowed_headers' => ['*'],
 
